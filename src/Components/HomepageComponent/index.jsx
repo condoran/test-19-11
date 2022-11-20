0ea1ec6af8469c4a679c0bb6b9bb293d45675da3
condoran <44895249+condoran@users.noreply.github.com>
2022-11-20 15:46:09 +0200

    update styling and pagination

--- src/Components/HomepageComponent/index.jsx
+++ src/Components/HomepageComponent/index.jsx
@@ -39,16 +39,18 @@ const HomepageComponent = (props) => {
             <Box sx={{margin: "40px 0", border: "1px solid white", borderRadius: "5px", paddingBottom: "50px", position: "relative"}}>
                 {gists.length > 0 ? (gists.map((el) => {
                     return Object.values(el?.files).length > 0 && Object.values(el?.files)?.map((elem) => {return (
-                        <Box onClick={() => {window.open(elem?.raw_url, '_blank', 'noopener,noreferrer');}} sx={{display: "grid", gridTemplateColumns: "1fr 1fr 1fr", cursor: "pointer", padding: "10px", gap: "20px", borderBottom: "1px solid white" }} key={el?.id}>
+                        <Box onClick={() => {window.open(elem?.raw_url, '_blank', 'noopener,noreferrer');}} sx={{display: "grid", gridTemplateColumns: "1fr 35px 100px", cursor: "pointer", padding: "10px", gap: "20px", borderBottom: "1px solid white" }} key={el?.id}>
                             <Box>{elem?.filename || "-"}</Box>
                             <Box sx={{display: "flex", color: "white", border: "1px solid #36A1FF", width: "30px", height: "30px", borderRadius: "50%", backgroundColor: "#36A1FF", textAlign: "center", alignItems: "center", justifyContent: "center", alignContent: "center", }}>{extensionTypeMap[elem?.type] || "-"}</Box>
                             <Box>{el?.history ? el?.history.map((history) => {return <Box><Tooltip title="Delete"><Box component="img" sx={{borderRadius: "50%"}} width="30px" src={history?.user?.avatar_url} /></Tooltip></Box>}) : (<Tooltip title={el?.owner?.login}><Box component="img" sx={{borderRadius: "50%"}} width="25px" src={el?.owner?.avatar_url} /></Tooltip>)}</Box>
                         </Box>
                         )})}
                 )) : (<Box sx={{display: "grid", gridTemplateColumns: "110px", padding: "10px", justifyContent: "center", borderBottom: "1px solid white" }}>No gists found!</Box>)}
-                <Box sx={{position: "absolute", bottom: "15px", right: "10px", display: "flex"}}>
+                <Box sx={{position: "absolute", bottom: "7px", right: "10px", display: "flex"}}>
                     <Box>Rows: <Input value={rows} sx={{paddingLeft: "7px", width: "40px", color: "white", '&::before': {borderColor: "white"}}} onChange={(e) => {setRows(e.target.value)}} /></Box>
                     <Box>Page: <Input value={page} sx={{paddingLeft: "7px", width: "40px", color: "white", '&::before': {borderColor: "white"}}} onChange={(e) => {setPage(e.target.value)}} /></Box>
+                    <Button sx={{color: "white"}} onClick={() => {setPage(parseInt(page) - 1)}}>{"<"}</Button>
+                    <Button  sx={{color: "white"}} onClick={() => {setPage(parseInt(page) + 1)}}>{">"}</Button>
                 </Box>
             </Box>
         </Box>
