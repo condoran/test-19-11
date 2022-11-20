a249f9dfd1ef8fd3e7e595e89e3c0c0f1b5682f8
condoran <44895249+condoran@users.noreply.github.com>
2022-11-20 15:37:21 +0200

    search function and pagination

--- src/Components/HomepageComponent/index.jsx
+++ src/Components/HomepageComponent/index.jsx
@@ -1,124 +1,22 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import {useDispatch, useSelector} from "react-redux"
 import { fetchGists, fetchOneGist } from "../../store/gists/thunks";
-import { Box, Input, Typography } from '@mui/material';
-
-var extensionTypeMap = {
-    "video/3gpp": "3gp",
-    "application/octet-stream": "so",
-    "application/postscript": "ps",
-    "audio/x-aiff": "aiff",
-    "application/pgp-signature": "sig",
-    "video/x-ms-asf": "asx",
-    "text/x-asm": "s",
-    "application/atom+xml": "atom",
-    "audio/basic": "snd",
-    "video/x-msvideo": "avi",
-    "application/x-msdownload": "msi",
-    "image/bmp": "bmp",
-    "application/x-bzip2": "bz2",
-    "text/x-c": "hh",
-    "application/vnd.ms-cab-compressed": "cab",
-    "application/vnd.ms-htmlhelp": "chm",
-    "text/plain": "txt",
-    "application/x-x509-ca-cert": "pem",
-    "text/css": "css",
-    "text/csv": "csv",
-    "application/x-debian-package": "deb",
-    "text/x-diff": "diff",
-    "image/vnd.djvu": "djvu",
-    "application/msword": "dot",
-    "application/xml-dtd": "dtd",
-    "application/x-dvi": "dvi",
-    "application/java-archive": "war",
-    "message/rfc822": "mime",
-    "text/x-fortran": "for",
-    "video/x-flv": "flv",
-    "text/x-script.ruby": "ru",
-    "image/gif": "gif",
-    "application/x-gzip": "gz",
-    "text/html": "html",
-    "image/vnd.microsoft.icon": "ico",
-    "text/calendar": "ifb",
-    "text/x-java-source": "java",
-    "application/x-java-jnlp-file": "jnlp",
-    "image/jpeg": "jpg",
-    "application/javascript": "js",
-    "application/json": "json",
-    "audio/x-mpegurl": "m3u",
-    "video/mp4": "mp4v",
-    "text/troff": "tr",
-    "application/mathml+xml": "mml",
-    "application/mbox": "mbox",
-    "audio/midi": "midi",
-    "video/x-mng": "mng",
-    "video/quicktime": "qt",
-    "audio/mpeg": "mp3",
-    "video/mpeg": "mpg",
-    "application/vnd.oasis.opendocument.presentation": "odp",
-    "application/vnd.oasis.opendocument.spreadsheet": "ods",
-    "application/vnd.oasis.opendocument.text": "odt",
-    "application/ogg": "ogg",
-    "text/x-pascal": "pas",
-    "image/x-portable-bitmap": "pbm",
-    "application/pdf": "pdf",
-    "image/x-portable-graymap": "pgm",
-    "application/pgp-encrypted": "pgp",
-    "text/x-script.perl": "pl",
-    "text/x-script.perl-module": "pm",
-    "image/png": "png",
-    "image/x-portable-anymap": "pnm",
-    "image/x-portable-pixmap": "ppm",
-    "application/vnd.ms-powerpoint": "ppt",
-    "image/vnd.adobe.photoshop": "psd",
-    "text/x-script.python": "py",
-    "audio/x-pn-realaudio": "ram",
-    "application/x-rar-compressed": "rar",
-    "application/rdf+xml": "rdf",
-    "application/x-redhat-package-manager": "rpm",
-    "application/rss+xml": "rss",
-    "application/rtf": "rtf",
-    "text/sgml": "sgml",
-    "application/x-sh": "sh",
-    "image/svg+xml": "svgz",
-    "application/x-shockwave-flash": "swf",
-    "application/x-tar": "tar",
-    "application/x-bzip-compressed-tar": "tbz",
-    "application/x-tcl": "tcl",
-    "application/tei+xml": "tei",
-    "application/x-tex": "tex",
-    "application/x-texinfo": "texinfo",
-    "image/tiff": "tiff",
-    "application/x-bittorrent": "torrent",
-    "text/x-vcard": "vcf",
-    "text/x-vcalendar": "vcs",
-    "model/vrml": "wrl",
-    "audio/x-wav": "wav",
-    "audio/x-ms-wma": "wma",
-    "video/x-ms-wmv": "wmv",
-    "video/x-ms-wmx": "wmx",
-    "application/wsdl+xml": "wsdl",
-    "image/x-xbitmap": "xbm",
-    "application/xhtml+xml": "xhtml",
-    "application/vnd.ms-excel": "xls",
-    "application/xml": "xsl",
-    "image/x-xpixmap": "xpm",
-    "application/xslt+xml": "xslt",
-    "text/yaml": "yml",
-    "application/zip": "zip"
-}
+import { Box, Button, Input, Typography, Tooltip } from '@mui/material';
+import extensionTypeMap from "../../utils/filetypes";
 
 const HomepageComponent = (props) => {
     const dispatch = useDispatch();
+    const [username, setUsername] = useState(null);
+    const [rows, setRows] = useState(30);
+    const [page, setPage] = useState(1);
     const gists = useSelector((state) => state.gists.gists);
 
     
     
     useEffect(() => {
-        dispatch(fetchGists({user: "emreoemreo"}));
-        
-        dispatch(fetchOneGist());
-    }, []);
+        if (username)
+            dispatch(fetchGists({user: username, page: page, per_page: rows}));
+    }, [rows, page]);
 
     return (
         <Box>
@@ -130,23 +28,28 @@ const HomepageComponent = (props) => {
                 alignItems: "center",
                 flexDirection: "column",
                 gap: "10px",
-            }}>
+            }}
+            >
                 <Typography variant="p" gutterBottom>
                     Just search by github username
                 </Typography>
-                <Input id="outlined-basic" label="Outlined" variant="outlined" />
+                <Input value={username} onChange={(e) => {setUsername(e.target.value)}} sx={{color: "white", '&::before': {borderColor: "white"}}} id="outlined-basic" label="Outlined" variant="outlined" />
+                <Button onClick={() => {dispatch(fetchGists({user: username, page: page, per_page: rows}))}}>Search</Button>
             </Box>
             <Box sx={{margin: "40px 0", border: "1px solid white", borderRadius: "5px", paddingBottom: "50px", position: "relative"}}>
-                {gists.map((el) => {
+                {gists.length > 0 ? (gists.map((el) => {
                     return Object.values(el?.files).length > 0 && Object.values(el?.files)?.map((elem) => {return (
                         <Box onClick={() => {window.open(elem?.raw_url, '_blank', 'noopener,noreferrer');}} sx={{display: "grid", gridTemplateColumns: "1fr 1fr 1fr", cursor: "pointer", padding: "10px", gap: "20px", borderBottom: "1px solid white" }} key={el?.id}>
                             <Box>{elem?.filename || "-"}</Box>
-                            <Box sx={{display: "flex", color: "white", border: "1px solid #36A1FF", width: "25px", height: "25px", borderRadius: "50%", backgroundColor: "#36A1FF", textAlign: "center", alignItems: "center", justifyContent: "center", alignContent: "center", }}>{extensionTypeMap[elem?.type] || "-"}</Box>
-                            <Box>{el?.history ? el?.history.map((history) => {return <Box><Box component="img" sx={{borderRadius: "50%"}} width="25px" src={history?.user?.avatar_url} /></Box>}) : (<Box component="img" sx={{borderRadius: "50%"}} width="25px" src={el?.owner?.avatar_url} />)}</Box>
+                            <Box sx={{display: "flex", color: "white", border: "1px solid #36A1FF", width: "30px", height: "30px", borderRadius: "50%", backgroundColor: "#36A1FF", textAlign: "center", alignItems: "center", justifyContent: "center", alignContent: "center", }}>{extensionTypeMap[elem?.type] || "-"}</Box>
+                            <Box>{el?.history ? el?.history.map((history) => {return <Box><Tooltip title="Delete"><Box component="img" sx={{borderRadius: "50%"}} width="30px" src={history?.user?.avatar_url} /></Tooltip></Box>}) : (<Tooltip title={el?.owner?.login}><Box component="img" sx={{borderRadius: "50%"}} width="25px" src={el?.owner?.avatar_url} /></Tooltip>)}</Box>
                         </Box>
                         )})}
-                )}
-                <Box sx={{position: "absolute", bottom: "15px", right: "10px"}}>1/2</Box>
+                )) : (<Box sx={{display: "grid", gridTemplateColumns: "110px", padding: "10px", justifyContent: "center", borderBottom: "1px solid white" }}>No gists found!</Box>)}
+                <Box sx={{position: "absolute", bottom: "15px", right: "10px", display: "flex"}}>
+                    <Box>Rows: <Input value={rows} sx={{paddingLeft: "7px", width: "40px", color: "white", '&::before': {borderColor: "white"}}} onChange={(e) => {setRows(e.target.value)}} /></Box>
+                    <Box>Page: <Input value={page} sx={{paddingLeft: "7px", width: "40px", color: "white", '&::before': {borderColor: "white"}}} onChange={(e) => {setPage(e.target.value)}} /></Box>
+                </Box>
             </Box>
         </Box>
     );
--- src/store/gists/thunks.js
+++ src/store/gists/thunks.js
@@ -19,22 +19,3 @@ export const fetchGists = createAsyncThunk("fetchGists", async ({page = null, pe
         return thunkAPI.rejectWithValue({error: true, message: "SomethingWentWrong"});
     }
 })
-
-export const fetchOneGist = createAsyncThunk("fetchOneGist", async ({}, thunkAPI) => {
-    const options = {
-        url: `https://gist.githubusercontent.com/emreoemreo/f3be3fd2d3ab34da6cfd9988841abaec/raw/4bd45e606c3fa24fea9b56d0cca15d99e679daa9/UIPanelFader`,
-        method: "GET",
-    }
-
-    try {
-        console.log("here")
-        const response = await axios(options);
-        const data = response.data;
-        console.log(response);
-        // thunkAPI.dispatch(setGists({ gists: data }));
-    } catch (e)
-    {
-        console.log(e);
-        return thunkAPI.rejectWithValue({error: true, message: "SomethingWentWrong"});
-    }
-})
